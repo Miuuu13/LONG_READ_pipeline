4cb7cd11f6c765e884fc87c5108d9d3551fa57b2
Manuela Andrea Beyer <67337290+Miuuu13@users.noreply.github.com>
2024-03-09 17:02:28 +0100

    Update LONG_READ_1_Generate_RNA_long_read.py
    9MRC24

--- LONG_READ_1_Generate_RNA_long_read.py
+++ LONG_READ_1_Generate_RNA_long_read.py
@@ -1,57 +1,113 @@
 #%%
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
+from scipy import signal
 import os
+import pandas as pd
 
-directory_path = r'C:\Users\manue\MASTER_PROJECT_RNA_seq_data\Optimize_ML_simulated_RNA_sequencing_data-main\Optimize_ML_simulated_RNA_sequencing_data-main\LONG_READ_training_data'
+""" Script to generate LONG READS (6000 time points), noise level 2 %, batch_size = 32 """
 
-#npz_files = [f for f in os.listdir(directory_path) if f.endswith('.npz')]
+base_path = os.getcwd() # working directory # in main
+path_save_long_read = os.path.join(base_path, r'LONG_READ_training_data')
+# Check if the save path exists, if not, create it
+if not os.path.exists(path_save_long_read ):
+    os.makedirs(path_save_long_read )
 
-def plot_generated_long_read(signal_train, map_onehot, rand_seq_numeric, file_name):
-    # Convert numeric sequence to string using a predefined dictionary
-    base_dict = {0: "A", 1: "C", 2: "G", 3: "T"}
-    rand_seq = ''.join([base_dict[num] for num in rand_seq_numeric])
+# Acess kmer table to get values for the 5-mers
+kmer_info = pd.read_csv('template_median68pA_200mv.txt', delim_whitespace=True)
+kmer_info = kmer_info.values
+
+def Generate_sim_signal(N_batch, batch_size , path, kmer_info, seq_len , time_points):
+    """ Function to generate the LONG READ """
     
-    fig, axs = plt.subplots(3, 1, figsize=(14, 20), constrained_layout=True)
+    seq_len = seq_len + 5
+    labels = 5
+    kmer_length = 5
 
-    # Plot Signal Train
-    axs[0].plot(signal_train)
-    axs[0].set_title('Signal Train')
+    for j in range (N_batch):
+        print(j)
+        Sim_raw_signal_tot = np.zeros([batch_size,time_points])
+        Map_raw_signal_tot = np.zeros([batch_size,time_points,labels])
 
-    # PContour plot for map_onehot
-    if map_onehot.any():
-        axs[1].contourf(map_onehot.T, cmap='viridis', levels=np.linspace(0, 1, num=50))
-        axs[1].set_title('Contour plot of One-hot Encoded Map')
-        axs[1].set_xlabel('Time poinst')
-        axs[1].set_ylabel('Nucleotide position')
+        for k in range(batch_size):
+            # generate random time step to read k-mer
+            Rand_seq = np.random.randint(0,4, seq_len)
+            rand_seq_base =np.empty(seq_len, dtype=str)
+            Sim_raw_signal = np.zeros([time_points])
+            Map_raw_signal_S = np.zeros([time_points,labels])
 
-    # Heatmap using Seaborn
-    sns.heatmap(map_onehot.T, cmap="YlGnBu", cbar_kws={'label': 'Feature Activation'}, ax=axs[2])
-    axs[2].set_title('Map Onehot Features (Bases) over time points')
-    axs[2].set_xlabel('Time Points')
-    axs[2].set_ylabel('Features/Bases and Spacer')
-    # Adjusting the y-ticks to show all feature labels
-    axs[2].set_yticks(range(map_onehot.shape[1]))
-    axs[2].set_yticklabels([f'Feature {i}' for i in range(map_onehot.shape[1])]) #change to letter, spacer later
+            # dictionary version one
+            base_dict = { 0:"A", 1:"C", 2:"G", 3:"T"}
+            base_dict_2 = { "A":0, "C":1, "G":2, "T":3}
 
-    plt.suptitle(file_name)
-    plt.show()
-    # sequence as a string
-    print(f"Random Sequence for {file_name}: {rand_seq}")
-    print(f"Signal Train for {file_name}:\n", signal_train)
-    print(f"Map Onehot for {file_name}:\n", map_onehot)
+            for i in range(len(Rand_seq)):
+                rand_seq_base[i] = base_dict[Rand_seq[i]]
+            probe_raw_data = 0
+            mu, sigma = 3.686, 0.4254
 
-# Loop through each file and plot (currently one 1 npz file)
-npz_files = [f for f in os.listdir(directory_path) if f.endswith('.npz')]
-for npz_file in npz_files:
-    file_path = os.path.join(directory_path, npz_file)
-    data = np.load(file_path)
-    signal_train = data['signal_train'][0] 
-    map_onehot = data['map_onehot'][0]  
-    rand_seq_numeric = data['rand_seq']  
+            for i in range(seq_len - kmer_length + 1):
+                #obtain k-mer Raw-signal value
+                if i > 0:
+                    #find the k-mer value in the table and associate the specific signal value
+                    Single_kmer_vector = rand_seq_base[i: i + kmer_length]
+                    Single_kmer = ''.join(rand_seq_base[i: i + kmer_length])
+                    probe_kmer = np.where(kmer_info[:,0] == Single_kmer)[0]
 
-plot_generated_long_read(signal_train, map_onehot, rand_seq_numeric, npz_file)
+                    # k-mer signal depends on a gaussian distrbution center on a specific value
+                    # generate here the k-mer distribution and create the k-mer signal from it
+                    kmer_center = kmer_info[probe_kmer[0],1]
+                    kmer_std = (1/4)*kmer_info[probe_kmer[0],2]
+                    Signal_kmer = np.random.normal(kmer_center, kmer_std)
+
+                    # from the distribution, obtain the number of time points that the k-mer is present in the signal
+                    N_step = int(np.random.lognormal(mu, sigma))
+
+                    while N_step < 20:
+                        N_step = int(np.random.lognormal(mu, sigma))
+
+                    Sim_raw_signal[probe_raw_data : probe_raw_data + N_step] = Signal_kmer
+
+                    Spc = 3
+                    if i == 1:
+
+                        if np.random.randint(2) == 0:
+                            Map_raw_signal_S[probe_raw_data : probe_raw_data + N_step - Spc, base_dict_2[Single_kmer_vector[-1]]] = 1
+                            Map_raw_signal_S[probe_raw_data + N_step - Spc : probe_raw_data + N_step, -1] = 1
+
+                        else:
+                            Map_raw_signal_S[probe_raw_data + Spc : probe_raw_data + N_step -Spc, base_dict_2[Single_kmer_vector[-1]]] = 1
+                            #this is for adding blank signal
+                            Map_raw_signal_S[0 : Spc, -1] = 1
+                            Map_raw_signal_S[probe_raw_data + N_step -Spc : probe_raw_data + N_step, -1] = 1
+
+                    else:
+
+                        Map_raw_signal_S[probe_raw_data + Spc : probe_raw_data + N_step -Spc, base_dict_2[Single_kmer_vector[-1]]] = 1
+
+                        #this is for adding blank signal
+                        Map_raw_signal_S[probe_raw_data : probe_raw_data + Spc, -1] = 1
+                        Map_raw_signal_S[probe_raw_data + N_step -Spc : probe_raw_data + N_step, -1] = 1
+
+                    probe_raw_data += N_step
+
+            # filter frequency
+            filtered_signal = signal.savgol_filter(Sim_raw_signal, 11, 1) #was 10, 1 must be odd
+
+            # add noise, 2 % for LONG READ
+            noise_level = 0.02 
+            Sim_signal_final = filtered_signal + np.max(filtered_signal)*np.random.normal(0,noise_level, len(filtered_signal))
+
+            # code for the median. for dorado they use median
+            median = np.median(Sim_signal_final)
+            std_med = np.median(np.abs(Sim_signal_final-median))*1.4826 + np.finfo(np.float32).eps
+            Sim_signal_final = (Sim_signal_final - median)/std_med
+
+            Sim_raw_signal_tot[k] = Sim_signal_final #filtered_signal
+            Map_raw_signal_tot[k] = Map_raw_signal_S
+
+        file_name = "train_data_{}.npz".format(j);
+
+        np.savez_compressed(os.path.join(path,file_name), signal_train=Sim_raw_signal_tot, map_onehot=Map_raw_signal_tot, rand_seq = Rand_seq)
+
 
 
 # %%

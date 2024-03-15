98a71b9451c321deca83812c8f18e4a18dc8fa90
Manuela Andrea Beyer <67337290+Miuuu13@users.noreply.github.com>
2024-03-15 14:19:26 +0100

    Update LONG_READ_4_Rearrange.py
    15MRC24

--- LONG_READ_4_Rearrange.py
+++ LONG_READ_4_Rearrange.py
@@ -13,12 +13,12 @@ def recombine_basecalling(input_dir, output_file_path):
     npz_files = sorted([f for f in os.listdir(input_dir) if f.endswith('.npz')],
                        key=lambda x: int(x.split('_')[-1].split('.')[0]))
 
-    # Initialize a list to hold the recombined basecalling data for each sample
+    # Initialize a list to hold the recombined basecalling data for each sample later
     recombined_data = []
 
     # Loop over each sample index
     for sample_idx in range(32):  # Assuming there are 32 samples as mentioned
-        # Initialize a list to hold fragments for the current sample
+        # Initialize a list to hold fragments for the current sample later 
         sample_fragments = []
 
         # Loop through each sorted npz file and extract the corresponding sample's basecalling
@@ -37,32 +37,5 @@ def recombine_basecalling(input_dir, output_file_path):
     # Save the recombined array to an npz file
     np.savez_compressed(output_file_path, recombined_basecalling=recombined_array)
 
-# Use the function to recombine basecalling data
-base_path = os.getcwd()
-input_dir = os.path.join(base_path, 'LONG_READ_training_data_basecalled')
-output_file_path = os.path.join(base_path, 'recombined_long_read_basecalling.npz')
-
-recombine_basecalling(input_dir, output_file_path)
-
 
 #%%
-path_recombied = r"C:\Users\manue\MASTER_PROJECT_RNA_seq_data\Optimize_ML_simulated_RNA_sequencing_data-main\Optimize_ML_simulated_RNA_sequencing_data-main\recombined_long_read_basecalling.npz"
-def print_npz_contents(file_path):
-    
-    data = np.load(file_path)
-    
-    # list all keys
-    print("Key in .npz file:")
-    for key in data.keys():
-        print(f"- {key}")
-    
-    # check content of each key
-    for key in data.keys():
-        print(f"\nContent of '{key}':")
-        content = data[key]
-        print(content)
-        
-        print(f"Data type: {content.dtype}, Shape: {content.shape}")
-
-print_npz_contents(path_recombied)
-
